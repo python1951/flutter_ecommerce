7aed316ca39735c5a6f3ca716aaddfb26326b3b1
python1951 <qamarha28@gmail.com>
2020-07-05 11:47:06 +0500

    Add files

---
+++ lib/widgets/products/products.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../../pages/product.dart';
+import './price_tag.dart';
+import 'price_tag.dart';
+import 'product_card.dart';
+class Products extends StatelessWidget {
+  final List<Map<String, dynamic>> products;
+
+  Products(this.products);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return products.length > 0
+        ? ListView.builder(
+            itemBuilder: (BuildContext context,int index)=>ProductCard(products[index],index),
+            itemCount: products.length,
+          )
+        : Center(
+            child: Text("Add Products"),
+          );
+  }
+}
